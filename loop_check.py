b52c8b231854cf6859269077aae2e6251b970814
summit haque <summit haque>
2016-06-03 21:13:32 +0600

    2nd commit

--- loop_check.py
+++ loop_check.py
@@ -1,8 +1,10 @@
 import numpy
 
 value = 0
-for num in range(211028,967082):
+for num in range(0,10):
     print('number is %d' % (num))
     value = value + 1
+
+    print('code changed a bit')
 
 print(value)
